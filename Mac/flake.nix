7bcc9efc9fa8eebeaa20f744a0acba4b87c9c204
Rohan O'Dwyer <166503624+rohanod@users.noreply.github.com>
2024-10-23 18:30:27 +0100

    Refactor Mac/flake.nix and start.sh script to include aliases for searching apps from spotlight and fetch Screensaver MP4 files

--- Mac/flake.nix
+++ Mac/flake.nix
@@ -67,6 +67,29 @@
             pkgs.vagrant
           ];
 
+
+          # Make aliases so I can search for apps from spotlight   
+          system.activationScripts.applications.text = let
+                  env = pkgs.buildEnv {
+                    name = "system-applications";
+                    paths = config.environment.systemPackages;
+                    pathsToLink = "/Applications";
+                  };
+                in
+                  pkgs.lib.mkForce ''
+                    # Set up applications.
+                    echo "setting up /Applications..." >&2
+                    rm -rf /Applications/Nix\ Apps
+                    mkdir -p /Applications/Nix\ Apps
+                    find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+                    while read src; do
+                      app_name=$(basename "$src")
+                      echo "copying $src" >&2
+                      ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+                    done
+                  '';
+
+
           #Fetch Screensaver MP4 files
           system.activationScripts.fetchScreensaverFiles = ''
           echo "Fetching Screensaver files..."
