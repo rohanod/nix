73e3060a30a2d99901a0d39e7530ad1a4f1ee214
Rohan O'Dwyer <166503624+rohanod@users.noreply.github.com>
2024-10-23 15:38:00 +0100

    Refactor installation scripts and update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Rohan's Customized Darwin Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
