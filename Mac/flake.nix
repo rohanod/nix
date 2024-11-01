0f14b2380e85ffdd6d8bfcb46ac21650bd8b514b
Rohan O'Dwyer <166503624+rohanod@users.noreply.github.com>
2024-11-01 14:20:13 +0100

    Refactor application setup script in Nix flake configuration to improve logging and ensure proper application linking

--- Mac/flake.nix
+++ Mac/flake.nix
@@ -114,22 +114,24 @@
 
           # Consolidated applications script
           system.activationScripts.applications.text = let
-            env = pkgs.buildEnv {
-              name = "system-applications";
-              paths = config.environment.systemPackages;
-              pathsToLink = "/Applications";
-            };
-          in
-            ''
-              echo "setting up /Applications/Nix Apps..."
-              rm -rf /Applications/Nix\ Apps
-              mkdir -p /Applications/Nix\ Apps
-              find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-              while read src; do
-                app_name=$(basename "$src")
-                ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-              done
-            '';
+                  env = pkgs.buildEnv {
+                    name = "system-applications";
+                    paths = config.environment.systemPackages;
+                    pathsToLink = "/Applications";
+                  };
+                in
+                  pkgs.lib.mkForce ''
+                    # Set up applications.
+                    echo "setting up /Applications..." >&2
+                    rm -rf /Applications/Nix\ Apps
+                    mkdir -p /Applications/Nix\ Apps
+                    find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+                    while read src; do
+                      app_name=$(basename "$src")
+                      echo "copying $src" >&2
+                      ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+                    done
+                  '';
 
           system.activationScripts.installPinokio.text = ''
             if [ ! -d "/Applications/Pinokio.app" ]; then
